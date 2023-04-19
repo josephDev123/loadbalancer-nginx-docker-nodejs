6d9d4aad61c377fb6ced8091cd684edf5ee07544
joseph uzuegbu <josephuzuegbu55@gmail.com>
2023-04-19 13:09:11 +0100

    removed error in Dockerfile and added a callback in the server.js file

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.use(express.json())
+
+app.get('/', (req, res)=> res.send('hello world'))
 
 
 app.listen(PORT, ()=> console.log(`App listening on port ${PORT}`))
