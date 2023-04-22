3879a0c81cafaeb31d14b0b663ec1801f11473fe
unknown <josephuzuegbu55@gmail.com>
2023-04-22 14:06:44 +0100

    changed the ngnix cofig upstream ports

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json())
 
-app.get('/', (req, res)=> res.send('hello world'))
+app.get('/', (req, res)=> res.send('hello world'));
 
 
 app.listen(PORT, ()=> console.log(`App listening on port ${PORT}`))
